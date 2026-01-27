bfd65947925dbf6396918145c91f5fd4a8488844
Alexander Svetlichniy <alexsvet036@gmail.com>
2026-01-28 01:20:39 +0300

    coco shnene

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.contrib import admin # noqa I005
+from django.urls import include, path # noqa I001
+from rest_framework import routers # noqa I001
 # noqa I004
 from api import views # noqa I001
 
